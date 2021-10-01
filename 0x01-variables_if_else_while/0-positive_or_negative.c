baa9045ed8224dfda777d6432bd1669037bc0e30
Guzman Caraballo <guzchelsea24@gmail.com>
2021-10-01 06:25:55 -0700

    adding a new script 0x01-1

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Holberton School project
+ *Return: always 0
+ **/
+int main(void)
+{
+		int n;
+
+			srand(time(0));
+				n = rand() - RAND_MAX / 2;
+					/* your code goes there */
+					return (0);
+}
