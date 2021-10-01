33f9fabfe1bbd8fa7421a6930668c30c334b0526
Guzman Caraballo <guzchelsea24@gmail.com>
2021-10-01 10:45:41 -0700

    adding a new script, 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,16 +2,15 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Holberton School project
- * Return: always 0
- **/
- int main(void)
- {
-                 int n, ld;
-		 srand(time(0));
-                        n = rand() - RAND_MAX / 2;
-                        (ld = n % 10);
-                        printf("Last digit of %d is %d and is 0\n", n, ld)
-			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld)
-			return (0);
-}                                                 
+* main - Holberton School project
+* Return: always 0
+**/
+int main(void)
+{
+char i;
+
+	for (i = 'a' ; i <= 'z'; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
