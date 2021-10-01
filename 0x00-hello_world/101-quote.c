4e3f6b7453fa478e9dd77ab9ba6674e76a596b18
Guzman Caraballo <guzchelsea24@gmail.com>
2021-09-30 19:25:42 -0700

    adding a new script (9)

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - Holberton School project
+ *Return: always 0
+ **/
+int main(void)
+	{
+	fprintf(stderr,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-109\n"
+		);
+	return (0);
+	}
