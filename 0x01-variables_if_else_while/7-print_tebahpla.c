6624e24328e687ec6d921a4248ca42aebe3ded15
Guzman Caraballo <guzchelsea24@gmail.com>
2021-10-01 16:11:27 -0700

    adding a new script, 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Holberton School project
+ * Return: end product
+ **/
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	putchar(i + 0);
+	putchar('\n');
+	return (0);
+}
