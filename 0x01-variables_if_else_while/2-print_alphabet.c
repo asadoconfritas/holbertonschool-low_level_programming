a00e136d08fbf86e8a6372b3bfb4457785dac1e5
Guzman Caraballo <guzchelsea24@gmail.com>
2021-10-01 09:58:36 -0700

    adding new scripts 0,1,2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,7 +10,7 @@
                  int n, ld;
 		 srand(time(0));
                         n = rand() - RAND_MAX / 2;
-                        (ld = n % 10)
+                        (ld = n % 10);
                         printf("Last digit of %d is %d and is 0\n", n, ld)
 			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld)
 			return (0);
