ec1191fe14884a4e22321f7226a61fd0c9944f67
Guzman Caraballo <guzchelsea24@gmail.com>
2021-10-01 11:31:50 -0700

    adding a new script, 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Holberton School project
+ * Return: end product
+ **/
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
