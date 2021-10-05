d427d7e8e390241439a0bc4523eb2e04525d08ee
Guzman Caraballo <guzchelsea24@gmail.com>
2021-10-05 09:48:00 -0700

    adding a new program

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Holberton School project.
+ * Return: end product
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-printalphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Holberton School project.
+ * Return: end product
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	putchar(i);
+	putchar('\n');
+}
