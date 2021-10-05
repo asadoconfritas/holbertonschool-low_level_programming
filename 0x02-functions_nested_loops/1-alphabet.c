c9d7b7bbcf2fa6a19eab22e8c53b797c611b24ba
Guzman Caraballo <guzchelsea24@gmail.com>
2021-10-05 10:13:44 -0700

    fix ex 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - Holberton School project.
  * Return: end product
  */
 
-void print_alphabet(void)
+int main(void)
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
 	putchar(i);
 	putchar('\n');
+	return (0);
 }
