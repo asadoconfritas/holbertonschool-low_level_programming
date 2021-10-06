41aa19e84d0a602e9ac17859c0187c1612527ce6
Guzman Caraballo <guzchelsea24@gmail.com>
2021-10-05 18:19:58 -0700

    adding a new script

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - Holberton School project.
- * Return: end product
- */
-
+ * main - check the code
+ *
+ * Return: Always 0.
+ **/
 int main(void)
 {
 	print_alphabet();
