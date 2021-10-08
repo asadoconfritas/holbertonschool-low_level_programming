59ad920388a591df9c920c315575076ae658bea7
Guzman Caraballo <guzchelsea24@gmail.com>
2021-10-07 18:51:17 -0700

    update main

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,13 @@
-#include <stdio.h>
-
 int _isupper(int c);
 
 int _isdigit(int c);
 
 int _putchar(char c);
+
+int mul(int a, int b);
+
+void print_numbers(void);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
