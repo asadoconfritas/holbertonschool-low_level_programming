ea770249c1c04c68be64ab6b03a764efc812922a
Guzman Caraballo <guzchelsea24@gmail.com>
2021-10-08 17:55:10 -0700

    adding new programs

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,12 +9,12 @@
 int IsPrime(long int n)
 
 {
-	{
 		if (n == 2 || n == 3)
 			return (1);
+
 		if (n <= 1 || n % 2 == 0 || n % 3 == 0)
 			return (0);
-	}
+
 		for (long int i = 5; i * i <= n; i += 6)
 		{
 			if (n % i == 0 || n % (i + 2) == 0)
@@ -24,5 +24,27 @@ int IsPrime(long int n)
 }
 
 /**
- *
- *
+ * main - print w printf
+ * Return: end program
+ **/
+
+int main(void)
+{
+	long int i, a;
+	int b;
+
+	b = 1;
+	i = 612852475143;
+	a = i / 2;
+	a--;
+	while (b)
+	{
+		printf("%ld\n", a);
+		if (((i % a) == 0) && (IsPrime(a) == 1))
+			b = 0;
+		else
+			a--;
+	}
+	printf("%ld\n", a);
+	return (0);
+}
