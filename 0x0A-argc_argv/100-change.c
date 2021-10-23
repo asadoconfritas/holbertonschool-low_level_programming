06942f1b3c077b60b39a632990749b0208afe51a
Guzman Caraballo <guzchelsea24@gmail.com>
2021-10-22 18:56:13 -0700

    adding n5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,3 +9,23 @@
  **/
 
 int main(int argc, char **argv)
+
+{
+	int vc, di, ci, ds, un, cambio;
+
+	if (argc == 2)
+	{
+		vc = atoi(argv[1]) / 25;
+		di = (atoi(argv[1]) - 25 * vc) / 10;
+		ci = (atoi(argv[1]) - 25 * vc - 10 * di) / 5;
+		ds = (atoi(argv[1]) % 5) / 2;
+		un = (atoi(argv[1]) % 5) % 2;
+		cambio = vc + di + ci + ds + un;
+		printf("%d\n", cambio);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
