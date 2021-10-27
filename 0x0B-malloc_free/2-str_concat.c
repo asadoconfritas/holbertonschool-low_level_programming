7a826f67ea010a966c4dd2a08121024a8d554523
Guzman Caraballo <guzchelsea24@gmail.com>
2021-10-26 19:55:41 -0700

    new 2 file

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: char
+ * @s2: char
+ * Return: end program
+ **/
+
+char *str_concat(char *s1, char *s2)
+
+{
+	char *p = NULL;
+
+	return (p);
+}
