7094f13ead40abf2e8990cad74a57ced7db29fb0
Guzman Caraballo <guzchelsea24@gmail.com>
2021-10-28 18:28:22 -0700

    ex 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,12 +1,27 @@
 #include "main.h"
-#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 /**
- * -
- *
- *
+ * _calloc - allocates mem w malloc -array-
+ * @nmemb: n of elements
+ * @size: in bytes
+ * Return: end program
  **/
 
+void *_calloc(unsigned int nmemb, unsigned int size)
+
 {
+	void *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+		return (NULL);
+
+	memset(p, 0, nmemb * size);
+	return (p);
 }
