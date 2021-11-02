7f740cdc628d136b220995c3945aa0e04612acbf
Guzman Caraballo <guzchelsea24@gmail.com>
2021-11-01 19:04:10 -0700

    update dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - takes 3 pieces of data
+ * @name: of the dog
+ * @age: of the dog
+ * @owner: name
+ * Return: end program
+ **/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
