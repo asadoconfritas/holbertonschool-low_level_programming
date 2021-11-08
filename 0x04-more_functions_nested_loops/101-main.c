728035b6bfb4d668547e8f11463db906c8cea925
Guzman Caraballo <guzchelsea24@gmail.com>
2021-11-08 10:48:00 -0800

    upd 101-

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -1,20 +1,21 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <limits.h>
 /**
  * main - check the code
  * Return: Always 0.
  */
 int main(void)
 {
-	print_number(98);
+	print_number(INT_MIN);
 	_putchar('\n');
-	print_number(402);
+	print_number(INT_MAX);
 	_putchar('\n');
-	print_number(1024);
+	printf("\nint_min: %d", INT_MIN);
 	_putchar('\n');
-	print_number(0);
+	printf("int max: %d", INT_MAX);
 	_putchar('\n');
-	print_number(-98);
+	print_number(-689796857);
 	_putchar('\n');
 	return (0);
 }
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,12 +7,14 @@
  **/
 void print_number(int n)
 {
+	int a;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		a = (n * -1);
 	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	if (a / 10)
+		print_number(a / 10);
+	_putchar(a % 10 + '0');
 }
