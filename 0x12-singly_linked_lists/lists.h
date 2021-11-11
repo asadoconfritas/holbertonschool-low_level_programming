d82217c85466249f49c84724f9cc6445f7f75f09
Guzman Caraballo <guzchelsea24@gmail.com>
2021-11-11 09:20:14 -0800

    upd header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,20 @@
 #ifndef HSLINKED_LISTS
 #define HSLINKED_LISTS
+#include <stddef.h>
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: of the string
+ * Description: singly linked list node
+ **/
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 size_t print_list(const list_t *h);
 
 size_t list_len(const list_t *h);
