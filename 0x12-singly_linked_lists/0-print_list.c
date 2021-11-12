f56580b4f7e27c3f563cf51e5d94cb7e7b4a7432
Guzman Caraballo <guzchelsea24@gmail.com>
2021-11-11 17:59:11 -0800

    0n1 updated

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - all elements of the list
+ * @h: pointer to list
+ * Return: n of nodes
+ **/
+size_t print_list(const list_t *h)
+{
+	size_t cont;
+
+	cont = 0;
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] (nil)\n", 0);
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		cont++
+	}
+	return (cont);
+}
---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len - count the elements of the list
+ * @h: pointer to list
+ * Return: n of nodes
+ **/
+size_t list_len(const list_t *h)
+{
+	size_t cont;
+
+	cont = 0;
+	while (h)
+	{
+		h = h->next;
+		cont++;
+	}
+	return (1 + cont);
+}
