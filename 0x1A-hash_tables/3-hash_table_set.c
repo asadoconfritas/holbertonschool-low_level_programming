f3e9cede48e8193484d435f1b89f6ca44b0ef263
root <guzchelsea24@gmail.com>
2022-01-28 18:47:31 -0800

    fix

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - add element
+ * @key: in the ht
+ * @value: associated to key
+ * @ht: hash table
+ * Return: end program
+ **/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	(void)ht;
+	(void)key;
+	(void)value;
+	return (0);
+}
